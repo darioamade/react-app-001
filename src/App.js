c698a31f09a24b008dbe997086455d4168e92349
darioamade <dario_isd@outlook.com>
2020-10-09 08:54:50 +0100

    added new components

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-    <h1>Darioamade</h1>
-    
+    <div>
+    <Router>
+    <Navbar/>
+    <Switch>
+    <Route path="/" exact />
+    </Switch>
+    </Router>
     </div>
   );
 }
